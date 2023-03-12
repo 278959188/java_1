fc6808e65c0fe1b3c158d98eedbc99f93e4690a3
278959188 <122278987+278959188@users.noreply.github.com>
2023-03-12 20:59:57 +0200

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int income = 70000; // доход
-        int spending = 100000; // расход
+        int eaters = 5; // сколько людей будут есть
 
-        int tax = income * 6 / 100; // налог 6% на доходы
-        int tax2 = (income - spending) * 15 / 100; // налог 15% на разницу доходов и расходов
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
 
-        System.out.println("Ваш налог на первой системе: " + tax + " рублей");
-        System.out.println("Ваш налог на первой системе: " + tax2 + " рублей");
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров воды");
+        System.out.println((potatoes / eaters) + " картофелин(а)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ка) специй");
 
     }
 }
